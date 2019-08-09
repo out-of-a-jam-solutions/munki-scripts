bfaf63c7faca5e394371e6145841ac70497664c0
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-08-09 12:35:56 -0800

    Updated client-install.sh to only require passing in the server URL. It will now download the latest version of munki and its conditional scripts to be installed.

--- client-install.sh
+++ client-install.sh
@@ -3,41 +3,30 @@
 #################
 ### ARGUMENTS ###
 #################
-# 1) munki installer package existing in the same directory as the install script, ex. munkitools-3.6.2.3776.pkg
-# 2) munki repository URL, ex. http://10.0.1.1/munki_repo
+# 1) munki repository URL, ex. http://10.0.1.1/munki_repo
 
 # environment
-MUNKI_PKG=$1
-SERVER_HOST="$2"
+SERVER_HOST="$1"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"  # https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 
 # install munki tools
-echo "installing $MUNKI_PKG"
-sudo installer -pkg $DIR/$MUNKI_PKG -target /
+echo "installing latest munki release"
+curl -L `curl -s https://api.github.com/repos/munki/munki/releases/latest | grep browser_download_url | cut -d '"' -f 4` -o $DIR/munki.pkg
+sudo installer -pkg $DIR/munki.pkg -target /
+
+# install munki conditional scripts
+echo "installing latest conditional scripts release"
+curl -L `curl -s https://api.github.com/repos/out-of-a-jam-solutions/munki-conditions-pkg/releases/latest | grep browser_download_url | cut -d '"' -f 4` -o $DIR/conditional-scripts.pkg
+sudo installer -pkg $DIR/conditional-scripts.pkg -target /
 
 # write the munki repo URL
 echo "Setting ManagedInstalls preference"
 sudo defaults write /Library/Preferences/ManagedInstalls SoftwareRepoURL $SERVER_HOST
 
-# show changed ManagedInstalls
-echo "ManagedInstalls preference is:"
-sudo defaults read /Library/Preferences/ManagedInstalls
-
-# copy the conditional install scripts
-sudo mkdir /usr/local/munki/conditions
-sudo cp $DIR/check-10.13-highsierra-compatibility.py /usr/local/munki/conditions/check-10.13-highsierra-compatibility.py
-sudo cp $DIR/check-10.14-mojave-compatibility.py /usr/local/munki/conditions/check-10.14-mojave-compatibility.py
-
-# set the conditional script permissions
-sudo chown root:wheel /usr/local/munki/conditions/*
-sudo chmod +x /usr/local/munki/conditions/*
-
 # create the munki bootstrap file
+echo "creating bootstrap file"
 touch /Users/Shared/.com.googlecode.munki.checkandinstallatstartup
 
-# sleep for 5 seconds
-echo "restarting in 5 seconds..."
-sleep 5
-
 # restart the computer
+echo "restarting..."
 sudo shutdown -r now
